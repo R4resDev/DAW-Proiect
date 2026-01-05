e00634660c68ea51b5260df620b13a06debb05dd
R4resDev <raresnecula05@gmail.com>
2026-01-05 23:58:30 +0200

    Refactor reCAPTCHA and email sending logic

--- verify_recaptcha.php
+++ verify_recaptcha.php
@@ -3,21 +3,15 @@ $returnMsg = '';
  
 if(isset($_POST['submit'])){ 
     
-	// Form fields validation check
     if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone'])){ 
          
-        // reCAPTCHA checkbox validation
         if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
-            // Google reCAPTCHA API secret key 
             $secret_key = '6LdBJCYsAAAAADqiSYvSfzowdGkAJ7Woxgxk0vtu'; 
              
-            // reCAPTCHA response verification
             $verify_captcha = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret_key.'&response='.$_POST['g-recaptcha-response']); 
             
-            // Decode reCAPTCHA response 
             $verify_response = json_decode($verify_captcha); 
              
-            // Check if reCAPTCHA response returns success 
             if($verify_response->success){ 
                 
                 $name = $_POST['name']; 
@@ -45,22 +39,16 @@ if(isset($_POST['submit'])){
                     $mail->SMTPSecure = "ssl";                 
                     $mail->Host       = "mail.rnecula.daw.ssmr.ro";
                     $mail->Port       = 465;
-                    $mail->Username   = $username; // Din mail_config.php
-                    $mail->Password   = $password; // Din mail_config.php
+                    $mail->Username   = $username;
+                    $mail->Password   = $password;
 
-                    // --- MODIFICĂRILE CHEIE AICI ---
 
-                    // 1. EXPEDITORUL fix: Trebuie să fie adresa serverului tău pentru a fi acceptat
                     $mail->SetFrom('rneculas@rnecula.daw.ssmr.ro', 'Fitness Contact - ' . $name); 
 
-                    // 2. DESTINATARUL fix: Unde vrei să primești tu mesajele
                     $mail->AddAddress('raresnecula05@gmail.com', 'Necula Rares'); 
 
-                    // 3. REPLY-TO: Aceasta este adresa utilizatorului din formular. 
-                    // Când apeși "Reply" în Gmail, vei răspunde automat acestei adrese.
                     $mail->AddReplyTo($email, $name); 
 
-                    // ------------------------------
 
                     $mail->Subject = 'Formular contact site';
                     $mail->AltBody = 'To view this post you need a compatible HTML viewer!';
@@ -72,7 +60,7 @@ if(isset($_POST['submit'])){
                 }
                 catch (phpmailerException $e)
 				{
-					echo $e->errorMessage(); //error from PHPMailer
+					echo $e->errorMessage();
 				}
 				 
             } 
