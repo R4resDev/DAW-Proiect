b712502bc5d5178f4bac3c877daa73de52203ed4
R4resDev <raresnecula05@gmail.com>
2026-01-05 23:59:53 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,12 @@
 <?php
 // functions.php
 
-/**
- * Protecție XSS: Curăță textul pentru afișarea în HTML
- */
+
 function e($string) {
     return htmlspecialchars($string ?? '', ENT_QUOTES, 'UTF-8');
 }
 
-/**
- * Generează un token CSRF pentru formulare
- */
+
 function generate_csrf_token() {
     if (empty($_SESSION['csrf_token'])) {
         $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
@@ -18,9 +14,7 @@ function generate_csrf_token() {
     return $_SESSION['csrf_token'];
 }
 
-/**
- * Verifică validitatea unui token CSRF
- */
+
 function verify_csrf_token($token) {
     return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
 }
